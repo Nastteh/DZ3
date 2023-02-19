96f4c046df1f75bd4a184ece7f61a3ea85adc554
«AnastasiyaLebedeva» <lmehanik72@mail.ru>
2023-02-19 18:37:27 +0300

    программа для 3д отрезка обновлён

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -4,7 +4,6 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-Console.WriteLine("n Задача 21");
 int x1 = Coordinate("x", "A");
 int y1 = Coordinate("y", "A");
 int z1 = Coordinate("z", "A");
@@ -18,14 +17,13 @@ int Coordinate(string coorName, string pointName)
     return Convert.ToInt32(Console.ReadLine());
 }
 
-double Decision(int x1, int x2, 
-                int y1, int y2, 
-                int z1, int z2){
-  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
-                   Math.Pow((y2-y1), 2) + 
-                   Math.Pow((z2-z1), 2));
+double Decision(int x1, int x2, int y1, int y2, int z1, int z2)
+{
+    return Math.Sqrt(Math.Pow((x2 - x1), 2) +
+                     Math.Pow((y2 - y1), 2) +
+                     Math.Pow((z2 - z1), 2));
 }
 
-double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
+double segmentLength = Math.Round(Decision(x1, x2, y1, y2, z1, z2), 2);
 
 Console.WriteLine($"Длина отрезка  {segmentLength}");
